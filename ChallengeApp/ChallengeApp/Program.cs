8d820c69c509d789f54dbdf49c6602c31bbb7baa
Eurydyk <levi@interia.pl>
2023-08-06 18:51:34 +0200

    zadanie 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+char sex = 'f';  // f - female  /   m -male
+int age = 17;
+if (sex == 'f' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33 && sex == 'f')
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (age < 18 && sex == 'm')
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Poza kryteriami");
+}
